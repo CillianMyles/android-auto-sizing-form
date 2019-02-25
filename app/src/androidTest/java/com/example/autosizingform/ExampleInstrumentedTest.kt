cb9ac2c0b3def487bc6aa55c8b5e4787cbc05e50
Cillian Myles <mylsey4thewin@gmail.com>
2019-02-25 16:41:27 +0100

    Tidy up

--- app/src/androidTest/java/com/example/autosizingform/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/autosizingform/ExampleInstrumentedTest.kt
@@ -2,19 +2,19 @@ package com.example.autosizingform
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
