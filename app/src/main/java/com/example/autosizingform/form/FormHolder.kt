a8b637e7e57f899b5fad4b7658a72bebe99fcb35
Cillian Myles <mylsey4thewin@gmail.com>
2019-02-26 10:46:51 +0100

    Show/hide remove button when input field full/empty;

--- app/src/main/java/com/example/autosizingform/form/FormHolder.kt
+++ app/src/main/java/com/example/autosizingform/form/FormHolder.kt
@@ -1,8 +1,11 @@
 package com.example.autosizingform.form
 
 import android.support.v7.widget.RecyclerView
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import kotlinx.android.synthetic.main.form_list_item.view.input
+import kotlinx.android.synthetic.main.form_list_item.view.remove
 
 /**
  * Created by Cillian Myles on 26/02/2019.
@@ -10,6 +13,18 @@ import kotlinx.android.synthetic.main.form_list_item.view.input
  */
 
 class FormHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    init {
+        itemView.remove.visibility = View.INVISIBLE
+        itemView.input.addTextChangedListener(object : TextWatcher {
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            override fun afterTextChanged(s: Editable?) {
+                itemView.remove.visibility =
+                        if (s?.toString().isNullOrEmpty()) View.INVISIBLE else View.VISIBLE
+            }
+        })
+    }
 
     fun show(value: String?) {
         itemView.input.setText(value)
